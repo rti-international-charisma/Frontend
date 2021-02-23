24d014f51381fa9f04c488404ffe345c82fad4df
Akash Raje <akash.raje@gmail.com>
2021-02-23 14:51:48 +0530

    First commit Hello World

---
+++ android/app/src/main/kotlin/com/rti/charisma/charisma/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rti.charisma.charisma
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CharismaApp());
+}
+
+class CharismaApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Charisma',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello World");
+  }
+}
