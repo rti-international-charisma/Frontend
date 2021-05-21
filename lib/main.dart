4dc9e3a333304066ca3476acb6cf3207eb983864
Akash Raje <akash.raje@gmail.com>
2021-05-21 10:40:09 +0530

    Change default env for testing

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'constants.dart';
 
 void main() async {
 
-  const env = String.fromEnvironment('ENV', defaultValue: 'stage');
+  const env = String.fromEnvironment('ENV', defaultValue: 'prod');
   String apiBaseUrl = environment.variables[env]!['baseUrl'] ?? 'http://0.0.0.0:5000/api';
   String assetsUrl = environment.variables[env]!['assetsUrl'] ?? 'http://chari-loadb-150mi7h76f40q-0c42746b9ba8f8ab.elb.ap-south-1.amazonaws.com:8055';
   print('Starting App for $env with API_BASEURL : $apiBaseUrl and ASSETS_URL: $assetsUrl');
