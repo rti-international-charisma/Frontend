a815b73850d2a6f302adf37d1c45fa94eb5bdd5a
rahuldole <rdole@equalexperts.com>
2021-05-28 18:36:22 +0530

    Added some initial code to handle auto logout when usere session ends

--- lib/apiclient/api_client.dart
+++ lib/apiclient/api_client.dart
@@ -3,17 +3,23 @@ import 'dart:io';
 // import 'package:charisma/account/user_state_model.dart';
 // import 'package:charisma/common/charisma_appbar_widget.dart';
 // import 'package:charisma/common/shared_preference_helper.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 
 import 'dart:convert' as convert;
 
-class ApiClient {
+class ApiClient extends ChangeNotifier {
   final Client _client;
   final String _baseUrl;
   final Map<String, String> _headers;
+  bool isSessionActive = false;
 
   ApiClient(this._client, this._baseUrl) : _headers = {};
   ApiClient._(this._client, this._baseUrl, this._headers);
+
+  void setSessionState(bool isActive) {
+    isSessionActive = isActive;
+  }
 
   Future<T>? get<T>(String path) async {
     var api = _baseUrl.endsWith("/")
@@ -78,6 +84,10 @@ class ApiClient {
     );
 
     if (response.statusCode >= 200 && response.statusCode < 300) {
+      return convert.jsonDecode(response.body) as T;
+    } else if (response.statusCode == 401) {
+      setSessionState(false);
+      notifyListeners();
       return convert.jsonDecode(response.body) as T;
     }
 
