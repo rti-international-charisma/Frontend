67056a19bffc474d8c038e5271821d2d88931e91
Akash Raje <akash.raje@gmail.com>
2021-06-21 17:25:00 +0530

    Added Seekbar and buffering loader

--- lib/common/video_player_widget.dart
+++ lib/common/video_player_widget.dart
@@ -1,6 +1,13 @@
+import 'dart:async';
+import 'dart:core';
+
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:charisma/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+
+import '../logger.dart';
+import 'charisma_circular_loader_widget.dart';
 
 class VideoPlayerWidget extends StatefulWidget {
   final String videoUrl;
@@ -16,6 +23,8 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
   late VideoPlayerController _videoController;
   static Duration videoPosition = Duration.zero;
   var isPlaying = false;
+  final _durationState = StreamController<DurationState>();
+  final _bufferState = StreamController<bool>();
   @override
   void initState() {
     _videoController = VideoPlayerController.network(widget.videoUrl);
@@ -27,6 +36,7 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
           _videoController.initialize().then((value) {
             _videoController.seekTo(videoPosition);
             _videoController.play();
+            _videoController.addListener(videoEventListener);
           });
         });
       }
@@ -38,84 +48,170 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: AspectRatio(
-          aspectRatio: 16 / 9,
-          child: VideoPlayer(_videoController),
+      body: Stack(
+        children:[Center(
+            child:
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: VideoPlayer(_videoController),
+            ),
         ),
-      ),
-      floatingActionButton: Row(
-        children: [
-          SizedBox(
-            width: 20,
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 23),
+                    child: buildPlayPauseButton(),
+                  ),
+                  Expanded(
+                    child: _progressBar(),
+                  ),
+                  widget.isFullscreen
+                      ? Padding(
+                    padding: EdgeInsets.only(bottom: 23),
+                        child: buildExitFullScreenButton(context),
+                      )
+                      : Padding(
+                    padding: EdgeInsets.only(bottom: 23),
+                        child: buildEnterFullScreenButton(),
+                      ),
+                ]
+            )
           ),
-          IconButton(
-            onPressed: () {
-              if (!_videoController.value.isInitialized) {
-                setState(() {
-                  isPlaying = true;
-                  _videoController.initialize().then((value) {
-                    _videoController.play();
-                  });
-                });
-              } else {
-                if (videoPosition >= _videoController.value.position) {
-                  _videoController.seekTo(videoPosition);
+          _buildBufferBuilder()
+        ]
+      ),
+    );
+  }
+
+  StreamBuilder<bool> _buildBufferBuilder() {
+    return StreamBuilder<bool>(
+            stream: _bufferState.stream,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                if (snapshot.data!) {
+                  return CharismaCircularLoader();
                 }
               }
+              return Container();
+            }
+        );
+  }
 
-              setState(() {
-                if (_videoController.value.isPlaying) {
-                  isPlaying = false;
-                  _videoController.pause();
-                } else {
-                  isPlaying = true;
-                  _videoController.play();
-                }
-                videoPosition = _videoController.value.position;
-              });
-            },
-            icon: Icon(
-              isPlaying ? Icons.pause : Icons.play_arrow,
-              color: linkColor,
-              size: 30,
-            ),
+  StreamBuilder<DurationState> _progressBar() {
+    return StreamBuilder<DurationState>(
+      stream: _durationState.stream,
+      builder: (context, snapshot) {
+        final durationState = snapshot.data;
+        final progress = durationState?.progress ?? Duration.zero;
+        final buffered = durationState?.buffered ?? Duration.zero;
+        final total = durationState?.total ?? Duration.zero;
+        return  ProgressBar(
+          progress: progress,
+          total: total,
+          buffered: buffered,
+          onSeek: (Duration seekTo) {
+            _videoController.seekTo(seekTo);
+          },
+          thumbRadius: 8,
+          timeLabelTextStyle: TextStyle(
+            color: linkColor
           ),
-          widget.isFullscreen
-              ? IconButton(
-                  icon: Icon(
-                    Icons.fullscreen_exit,
-                    color: linkColor,
-                    size: 30,
-                  ),
-                  onPressed: () {
-                    // Before exiting fullscreen, pause the video and update the video position
-                    setState(() {
-                      isPlaying = false;
-                      _videoController.pause();
-                      videoPosition = _videoController.value.position;
-                      Navigator.pop(context);
-                    });
-                  },
-                )
-              : IconButton(
-                  icon: Icon(
-                    Icons.fullscreen,
-                    color: linkColor,
-                    size: 30,
-                  ),
-                  onPressed: () {
-                    // Before entering fullscreen, pause the video and update the video position
-                    setState(() {
-                      isPlaying = false;
-                      _videoController.pause();
-                      videoPosition = _videoController.value.position;
-                      showFullScreen();
-                    });
-                  },
-                ),
-        ],
+        );
+      },
+    );
+  }
+
+  IconButton buildPlayPauseButton() {
+    return IconButton(
+      onPressed: () {
+        if (!_videoController.value.isInitialized) {
+          setState(() {
+            isPlaying = true;
+            _videoController.initialize().then((value) {
+              _videoController.seekTo(videoPosition);
+              _videoController.play();
+
+              _videoController.addListener(videoEventListener);
+
+            });
+          });
+        } else {
+          if (videoPosition >= _videoController.value.position) {
+            _videoController.seekTo(videoPosition);
+          }
+        }
+
+        setState(() {
+          if (_videoController.value.isPlaying) {
+            isPlaying = false;
+            _videoController.pause();
+          } else {
+            isPlaying = true;
+            _videoController.play();
+          }
+          videoPosition = _videoController.value.position;
+        });
+      },
+      icon: Icon(
+        isPlaying ? Icons.pause : Icons.play_arrow,
+        color: linkColor,
+        size: 30,
       ),
+    );
+  }
+
+  void videoEventListener() {
+
+    if (_videoController.value.errorDescription != null) {
+      Logger.log('Error ${_videoController.value.errorDescription}');
+    }
+
+    _bufferState.sink.add(_videoController.value.isBuffering);
+
+    DurationState newPlayerDurationState = DurationState(
+        progress: _videoController.value.position,
+        buffered: _videoController.value.buffered.first.end,
+        total: _videoController.value.duration);
+    _durationState.sink.add(newPlayerDurationState);
+  }
+
+  IconButton buildEnterFullScreenButton() {
+    return IconButton(
+      icon: Icon(
+        Icons.fullscreen,
+        color: linkColor,
+        size: 30,
+      ),
+      onPressed: () {
+        // Before entering fullscreen, pause the video and update the video position
+        setState(() {
+          isPlaying = false;
+          _videoController.pause();
+          videoPosition = _videoController.value.position;
+          showFullScreen();
+        });
+      },
+    );
+  }
+
+  IconButton buildExitFullScreenButton(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        Icons.fullscreen_exit,
+        color: linkColor,
+        size: 30,
+      ),
+      onPressed: () {
+        // Before exiting fullscreen, pause the video and update the video position
+        setState(() {
+          isPlaying = false;
+          _videoController.pause();
+          videoPosition = _videoController.value.position;
+          Navigator.pop(context);
+        });
+      },
     );
   }
 
@@ -139,6 +235,21 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
   @override
   void dispose() {
     _videoController.dispose();
+    _durationState.close();
+    _bufferState.close();
     super.dispose();
   }
+}
+
+class DurationState {
+
+  final Duration progress;
+  final Duration buffered;
+  final Duration? total;
+
+  const DurationState({
+    required this.progress,
+    required this.buffered,
+    this.total,
+  });
 }
